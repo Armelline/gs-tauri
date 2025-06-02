a875be5f14054002e2a8e9369b0cbb9ca54d4794
Armelline <support@armelline.com>
2025-06-02 01:40:42 +0100

    Moved resources, added quit to main.rs.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,14 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn exit_app() {
+    std::process::exit(0);
 }
+
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![exit_app])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
